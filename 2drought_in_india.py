27d7439130b4b9f1959870d3b26cf24fd2d3c9cc
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-11-07 02:20:18 +0530

    Update 2drought_in_india.py

--- 2drought_in_india.py
+++ 2drought_in_india.py
@@ -18,25 +18,4 @@ plt.ylabel('people affected')
 plt.title('drought in india total damage') 
 plt.show() 
 
-
-plt.plot(drought_set['Year'], drought_set[drought_set.columns[4]]) 
-plt.xlabel('year')
-plt.ylabel('damage in USD') 
-plt.title('drought in india total cost in USD') 
-plt.show() 
-
-a= drought_set[drought_set.columns[4]]/ drought_set[drought_set.columns[3]]
-a*=100
-plt.plot(drought_set['Year'], a) 
-plt.xlabel('year')
-plt.ylabel('cost per person *100 ') 
-plt.title('drought in india per person cost in USD') 
-plt.show() 
-
-drought_set["cost per person "]= a/100
-
-print(drought_set[drought_set.columns[3]].mean()  )
-print(drought_set[drought_set.columns[4]].mean()  )
-print(drought_set[drought_set.columns[5]].mean()  )
-print(drought_set.info())
-#7
+##partial code
