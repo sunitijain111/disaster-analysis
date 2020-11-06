bfd967d0d438ff46c9111b307c5f0e17cd0fc5b9
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-11-07 02:21:18 +0530

    Update 4Flood_India.py

--- 4Flood_India.py
+++ 4Flood_India.py
@@ -10,33 +10,4 @@ flood_set.drop(flood_set.columns[0], inplace = True, axis= 1)
 flood_set.dropna(axis=0, inplace= True)
 
 import matplotlib.pyplot as plt 
-
-  
-plt.plot(flood_set['Year'], flood_set[flood_set.columns[3]]) 
-plt.xlabel('year')
-plt.ylabel('people affected') 
-plt.title('flood in india total damage') 
-plt.show() 
-
-
-plt.plot(flood_set['Year'], flood_set[flood_set.columns[4]]) 
-plt.xlabel('year')
-plt.ylabel('damage in USD') 
-plt.title('flood in india total cost in USD') 
-plt.show() 
-
-a= flood_set[flood_set.columns[4]]/ flood_set[flood_set.columns[3]]
-
-plt.plot(flood_set['Year'], a) 
-plt.xlabel('year')
-plt.ylabel('cost per person  ') 
-plt.title('flood in india cost per person in USD') 
-plt.show() 
-
-flood_set["cost per person "] =a
-
-print(flood_set[flood_set.columns[3]].mean()  )
-print(flood_set[flood_set.columns[4]].mean()  )
-print(flood_set[flood_set.columns[5]].mean()  )
-print(flood_set.info())
-#92
+#partial code
